b4b5e7b15330e1a55bd4e4a2ea61ae53ac60862f
Martin <martinligabue@gmail.com>
2021-03-08 17:12:05 +0100

    aaaaa

--- src/progettomp/Main.java
+++ src/progettomp/Main.java
@@ -1,10 +1,14 @@
 package progettomp;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		//tmp
-		System.out.println("Help");
+		System.out.println("Help\n");
+		for (int i=0; i<10; i++){
+			System.out.println("A");
+		}
 	}
 
 }
---
+++ tests/progettomp/MainTest.java
@@ -0,0 +1,14 @@
+package progettomp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testConstructorArgumentsAreAssigned() {
+		fail("10");
+	}
+
+}
